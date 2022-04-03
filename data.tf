b1633207280c84c4a659149f90cf5bea55ea56ce
Arnau Colominas <acolominasilla@gmail.com>
2022-04-03 13:28:59 +0200

    fmt

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@ data "template_file" "lambda-policy" {
   template = file("${path.module}/templates/lambda-policy.json.tpl")
 
   vars = {
-    aws_region      = data.aws_caller_identity.current.account_id
-    account_id      = data.aws_region.current.name
+    aws_region = data.aws_caller_identity.current.account_id
+    account_id = data.aws_region.current.name
   }
 }
